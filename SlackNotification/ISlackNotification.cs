3cbb1e877c018f6ec05d18c6137aa5fb469bfd01
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2018-07-26 16:52:31 -0300

    Add
    Adicionado projeto para enviar notificação em canal do Slack.

---
+++ SlackNotification/ISlackNotification.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SlackNotification
+{
+    public interface ISlackNotification
+    {
+        void Register(string token, string channel);
+        Task SendNotificationAsync(string userName, string text);
+    }
+}
---
+++ SlackNotification/SlackNotification.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SlackNotification
+{
+    public class SlackNotification : ISlackNotification
+    {
+        private static string _token;
+        private static string _channel;
+
+        public SlackNotification()
+        {
+            _token = string.Empty;
+            _channel = string.Empty;
+        }
+
+        public void Register(string token, string channel)
+        {
+            _token = token;
+            _channel = channel;
+        }
+
+        public async Task SendNotificationAsync(string userName, string text)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync($"https://slack.com/api/chat.postMessage?" +
+                    $"token={ _token }" +
+                    $"&channel={ _channel }" +
+                    $"&username={ userName }" +
+                    $"&text={ text }" +
+                    $"&pretty=1", null);
+            }
+        }
+    }
+}
